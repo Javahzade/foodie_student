c874f38174ca2394642555c499c19be6b6cbf2a9
Ayisa Musayeva <ayishamusayeva04@gmail.com>
2023-12-08 16:01:18 +0400

    navigation

--- android/app/src/main/java/com/foodie/MainActivity.java
+++ android/app/src/main/java/com/foodie/MainActivity.java
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -16,6 +18,11 @@ public class MainActivity extends ReactActivity {
     return "FOODIE";
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+  
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
