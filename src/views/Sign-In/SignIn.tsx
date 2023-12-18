fa014acecc65dc75687a12e2687ea5728d7e87a2
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2023-12-18 11:06:17 +0300

    Sign-In

--- src/views/Sign-In/SignIn.tsx
+++ src/views/Sign-In/SignIn.tsx
@@ -13,14 +13,12 @@ function Signin(){
                 <Image style={{marginRight:14}} source={require("../../assets/icons/Rectangle.png")}/>
             </View>
         </View> */}
-        <View style={styles.menu}>
-            <Text style={styles.menuText1}>Hello,</Text>
-            <Text style={styles.menuText2}>Welcome Back!</Text>
+        <View style={styles.Text}>
+            <Text style={styles.Text1}>Hello,</Text>
+            <Text style={styles.Text2}>Welcome Back!</Text>
         </View>
-        <View style={styles.mail}>
+        <View style={styles.mail_password}>
             <Text style={styles.email}>Email</Text>
-        </View>
-        <View style={styles.password}>
             <Text style={styles.enterpassword}>Enter Password</Text>
         </View>
         <View style={styles.signup}>
@@ -37,69 +35,55 @@ export default Signin
 
 const styles = StyleSheet.create({
     area:{
-        flex:1
-    },
-    header:{
-        flexDirection:"row",    
-        // marginHorizontal:25,
-        // marginTop:29,
-        alignItems:"center",
+        flex:1,
+        paddingHorizontal:30,
         justifyContent:"space-between"
     },
-    menu:{
-        top:50,
-    //     marginTop:50,
-        left:30
+    // header:{
+    //     flexDirection:"row",    
+    //     marginHorizontal:25,
+    //     marginTop:29,
+    //     alignItems:"center",
+    //     justifyContent:"space-between"
+    // },
+    Text:{
+        marginTop:50,
+        justifyContent:"center",
+        alignItems:"flex-start"
     },
-    menuText1:{
-        width:84,
-        height:45,
-        // top:94,
-        // left:30,
+    Text1:{
         fontSize:30,
         lineHeight:45,
         fontWeight:"600",
         color:"#000000"
     },
-    menuText2:{
-        width:155,
-        height:30,
-        // top:139,
-        // left:30,
+    Text2:{
         fontSize:20,
         fontWeight:"400",
         lineHeight:30,
         color:"#121212"
     },
-    mail:{
-        top:165,
-        left:30
+    mail_password:{
+        
     },
     email:{
-        width:38,
-        height:21,
         fontWeight:"400",
         fontSize:14,
         lineHeight:21,
         color:"#121212"
     },
-    password:{
-        top:240,
-        left:30
-    },
     enterpassword:{
-        width:107,
-        height:21,
+        marginTop:84,
         fontWeight:"400",
         fontSize:14,
         lineHeight:21,
         color:"#121212"
     },
     signup:{
-        top:500,
+        top:50,
         justifyContent:"center",
+        alignItems:"center",
         flexDirection:"row",
-        height:17,
         fontWeight:"500",
         fontSize:11,
         lineHeight:16.5
@@ -111,9 +95,7 @@ const styles = StyleSheet.create({
         color:"#FF9C00"
     },
     homeindicator:{
-        top:530,
-        width:135,
-        height:5,
-        left:12
+        justifyContent:"center",
+        alignItems:"center"
     }
 })
