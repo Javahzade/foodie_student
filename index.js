56f3be4bba61f9602a92b09f4fa2e2cd53578abe
Sedaqet Eliyeva <sedaqet.eliyeva04@gmail.com>
2023-12-19 14:48:10 +0300

    fix app

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import { App } from "./src/App";
 import {name as appName} from './app.json';
-import { SignIn } from './src/views/Authorization/SignIn';
 
-AppRegistry.registerComponent(appName, () => SignIn);
+AppRegistry.registerComponent(appName, () => App);
