9e569ef8b882e3b63de11809ea7962bb1efb27b0
nijat <nicatveliyev@gmail.com>
2023-12-19 15:51:30 +0400

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import HomeScreen from './views/HomeScreen';
 import WelcomeScreen from './views/WelcomeScreen';
+import { SignIn } from './views/Authorization/SignIn';
+import { SignUp } from './views/Authorization/SignUp';
 
 
 const Stack = createStackNavigator();
@@ -16,6 +18,8 @@ function App() {
         }}>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
 
       </Stack.Navigator>
     </NavigationContainer>
