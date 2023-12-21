756e8e4320066c78d2b9c0d530ecaebf30677abd
nijat <nicatveliyev@gmail.com>
2023-12-21 16:11:17 +0400

    firebase config

--- src/views/WelcomeScreen.tsx
+++ src/views/WelcomeScreen.tsx
@@ -1,74 +1,74 @@
-// import React from 'react';
-// import { useNavigation } from '@react-navigation/native';
-// import { StyleSheet, View, TouchableOpacity, ImageBackground, Image, Text } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, View, TouchableOpacity, ImageBackground, Image, Text } from 'react-native';
 
-// function WelcomeScreen() {
-//   //   const navigation = useNavigation();
-
-
-//   return (
-//     <View style={styles.main}>
-
-//       <ImageBackground source={require('../assets/images/background.jpg')} style={styles.image} />
-//       <View style={styles.content}>
-//         <Image style={styles.icon}
-//           source={require('../assets/icons/icon.png')}
-//         />
-//       <Text style={styles.text1}>Get Cooking</Text>
-//       </View>
-//       <TouchableOpacity style={styles.button}>
-//             <Text style={styles.buttonText}>Sign In</Text>
-//         </TouchableOpacity>
+function WelcomeScreen() {
+    const navigation = useNavigation();
 
 
-//     </View>
-//   )
-// }
-// export default WelcomeScreen
+  return (
+    <View style={styles.main}>
 
-// const styles = StyleSheet.create({
-//   main: {
-//     width: "100%",
-//     height: "100%",
+      <ImageBackground source={require('../assets/images/background.jpg')} style={styles.image} />
+      <View style={styles.content}>
+        <Image style={styles.icon}
+          source={require('../assets/icons/icon.png')}
+        />
+      <Text style={styles.text1}>Get Cooking</Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
 
-//   },
-//   image: {
-//     width: "100%",
-//     height: "100%",
-//   },
-//   icon: {
-//     width: 79,
-//     height: 120,
 
-//   },
-//   content: {
-//     alignItems: "center",
-//     position: "absolute",
-//     alignSelf: "center",
-//     top: 100,
+    </View>
+  )
+}
+export default WelcomeScreen
 
-//   },
-//   text1:{
-//     color:"white",
-//     fontSize:50,
-//     width:213,
-//     height:120,
-//     textAlign:"center",
-//     top:287,
-//   },
+const styles = StyleSheet.create({
+  main: {
+    width: "100%",
+    height: "100%",
+
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  icon: {
+    width: 79,
+    height: 120,
+
+  },
+  content: {
+    alignItems: "center",
+    position: "absolute",
+    alignSelf: "center",
+    top: 100,
+
+  },
+  text1:{
+    color:"white",
+    fontSize:50,
+    width:213,
+    height:120,
+    textAlign:"center",
+    top:287,
+  },
   
-//   button:{
-//     backgroundColor:"#129575",
-//     height:48,
-//     justifyContent:"center",
-//     alignItems:"center",
-//     borderRadius:10,
-//     marginBottom:16
-// },
-// buttonText:{
-//     fontWeight:"bold",
-//     color:"white",
-//     fontSize:16,
-//     flexDirection:"row",
-// },
-// })
+  button:{
+    backgroundColor:"#129575",
+    height:48,
+    justifyContent:"center",
+    alignItems:"center",
+    borderRadius:10,
+    marginBottom:16
+},
+buttonText:{
+    fontWeight:"bold",
+    color:"white",
+    fontSize:16,
+    flexDirection:"row",
+},
+})
